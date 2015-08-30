259909bc6151d76fe6223f7e1bd0459286c75af3
Ahmed Musawir <ahmedmusawir@Ahmeds-Mac.local>
2015-08-30 23:44:06 +0800

    2nd Commit: HTML5 video to a Div

---
+++ footer-vid-index.php
@@ -0,0 +1,31 @@
+		</div> <!-- Wrapper End -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="js/jquery.stellar.min.js"></script>
+
+		<!-- Bootstrap JavaScript -->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+		<!-- Custom JavaScript -->
+		<script src="js/jquery.tubular.1.0.js"></script>
+		<script src="js/app.js"></script>
+    	<!-- // <script type="text/javascript" charset="utf-8" src="js/index.js"></script> -->
+    	<script>
+
+    	<?php //$youtube =  'WTAHzO1i6Mw'; ?>
+
+    	jQuery('document').ready(function() {
+			var options = { videoId: '<?php echo $youtube; ?>', start: 3 };
+			// var options = { videoId: 'ab0TSkLe-E0', start: 3 };
+			// $('#myVideo').tubular(options);
+			// $('#wrapper').tubular(options);
+			// f-UGhWj1xww cool sepia hd
+			// 49SKbS7Xwf4 beautiful barn sepia
+		});
+
+    	</script>
+
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,42 +1,59 @@
 <?php include "header.php" ?>
+		<!-- <iframe id="video-bg"  src="https://www.youtube.com/embed/WTAHzO1i6Mw?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;modestbranding=1&amp;disablekb=1&amp;enablejsapi=1" frameborder="0" > -->
 
+		<!-- </iframe> -->
 
 
 		
 		<!-- <iframe id="video-bg" src="https://www.youtube.com/embed/WTAHzO1i6Mw?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;modestbranding=1&amp;disablekb=1&amp;enablejsapi=1" frameborder="0" ></iframe> -->
-	<div class="hero-unit clearfix" data-stellar-background-ratio="">
-<figure id="wrapper" class="clearfix">
-		<!-- <iframe id="video-bg"  src="https://www.youtube.com/embed/WTAHzO1i6Mw?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;modestbranding=1&amp;disablekb=1&amp;enablejsapi=1" frameborder="0" > -->
-		<!-- </iframe> -->
-</figure>	
-	
-	<header id="header" class="">
-		<!-- <img src="img/hero-bg.jpg" alt=""> -->
-		<h1 class="text-center">Hello World <br>
-		<small>subtext</small></h1>
-	</header><!-- /header -->
+	<div class="hero-unit clearfix" >
+		<header id="header" class="">
+			<h1 class="text-center">Hello World <br>
+			<small>subtext</small></h1>
+		</header>
 	</div>
 
 	<section class="first-section">
+	<h1>Section One</h1>
 		<p>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia odit atque cupiditate id nihil illo ipsam recusandae, odio esse, neque asperiores modi molestias? Officia vitae nemo adipisci, magni mollitia vel odit hic porro temporibus voluptates quibusdam nulla sint labore soluta veniam ipsa accusantium corporis, amet neque iure, impedit veritatis. Rem autem cumque molestiae possimus, deserunt explicabo nisi necessitatibus harum officiis ea nostrum velit perspiciatis, perferendis eaque quidem voluptas optio tempora dolores doloremque. Aperiam quos voluptatibus laboriosam eum, quaerat distinctio omnis ipsa alias maxime aspernatur corrupti magni dolorem eaque unde mollitia. Adipisci possimus delectus sapiente quod nostrum et fuga quasi ab nam, nesciunt esse repellat consectetur beatae nobis veniam, architecto harum. At magni consequuntur quod illo, aut distinctio dolorem nobis. Dolor, reiciendis ullam debitis, corporis officiis excepturi mollitia impedit suscipit itaque rerum quos illum nihil ratione, doloremque, autem eveniet est accusamus ex commodi iusto. Praesentium earum ut, ullam doloribus eos ab adipisci natus architecto quo aut delectus omnis consectetur suscipit nesciunt eius quisquam modi eaque dignissimos. Minus voluptatibus cum at totam architecto aut esse, doloremque vitae, ut officiis earum. Fugiat repellendus, fugit cumque velit odio quo facilis iusto consequuntur expedita aspernatur numquam magnam eveniet sunt temporibus amet earum recusandae autem, dicta vitae, et quos soluta voluptates deleniti. Ratione harum eos error, sequi sint, deleniti, soluta libero sapiente cum aliquam itaque tempore magnam ullam facere voluptate. Dolor obcaecati pariatur ipsam earum, praesentium iste inventore facere nostrum molestias, repellendus neque nisi magnam impedit maxime, illo itaque. Fuga, laboriosam voluptatem eligendi quisquam magni, at iure quia assumenda repellendus nobis praesentium rem. Ullam placeat aliquid sunt maiores laudantium. Est natus unde deserunt dolore facilis reiciendis vel provident quaerat commodi quos, cumque quibusdam amet tenetur maiores tempora expedita sapiente, tempore vitae doloribus officiis laudantium sequi accusamus praesentium minus? Provident ipsam saepe, molestiae autem doloribus repellat sint.	
 		</p>
 	</section>
 
 	<section class="second-section">
-		<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam nulla alias reprehenderit dolor nisi? Fugiat sequi autem atque illo ipsum praesentium, saepe sed, asperiores excepturi rem delectus reprehenderit error fugit dignissimos laborum porro laboriosam impedit, cum omnis iste esse at beatae quod? Illum nemo autem consequatur cumque amet aliquam ex vitae, earum cupiditate commodi tempore nobis ipsam impedit voluptatibus atque blanditiis totam illo dolore fugiat, neque voluptatum in, asperiores nam tempora quos. Non velit voluptas dicta nostrum veniam vel in eaque dolorem distinctio, est tempore id rerum inventore, fugiat adipisci recusandae saepe doloremque delectus harum quam! Commodi dolore laboriosam rem officiis soluta est delectus molestiae, pariatur fugiat necessitatibus voluptas praesentium voluptate perferendis ab, quae incidunt expedita obcaecati numquam consectetur quos eos nobis. Labore quaerat, officia vel dicta maxime, rerum, sequi eligendi quos culpa sapiente exercitationem repellat maiores voluptatibus aut. Recusandae sunt, error totam repudiandae unde, nisi reprehenderit, nesciunt similique animi reiciendis perspiciatis autem natus fugit excepturi asperiores! Ipsa, mollitia explicabo nisi aut perferendis at laborum, odio ipsum ipsam earum, omnis voluptatibus consectetur. Aperiam explicabo quas nemo. Nobis voluptate aliquam soluta, perspiciatis aut beatae, culpa, mollitia aspernatur maxime omnis praesentium nulla neque numquam inventore explicabo hic dolorum vero consectetur. Culpa, quam.</p>
+
+	<figure class="video-container">
+
+		<video autoplay loop muted poster="img/second-section.jpg" id="second-section-video">
+			<source src="video/video-4.webm" type="video/webm">
+			<source src="video/video-4.mp4" type="video/mp4">
+		</video>
+
+
+		<figcaption>explanatory caption</figcaption>
+		
+		
+		<div class="text-content">
+		<h1>Section Two</h1>
+
+			<h4>What's up</h4>
+			<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam nulla alias reprehenderit dolor nisi? Fugiat sequi autem atque illo ipsum praesentium, saepe sed, asperiores excepturi rem delectus reprehenderit error fugit dignissimos laborum porro laboriosam impedit, cum omnis iste esse at beatae quod? Illum nemo autem consequatur cumque amet aliquam ex vitae, earum cupiditate commodi tempore nobis ipsam impedit voluptatibus atque blanditiis totam illo dolore fugiat, neque voluptatum in, asperiores nam tempora quos. Non velit voluptas dicta nostrum veniam vel in eaque dolorem distinctio, est tempore id rerum inventore, fugiat adipisci recusandae saepe doloremque delectus harum quam! Commodi dolore laboriosam rem officiis soluta est delectus molestiae, pariatur fugiat necessitatibus voluptas praesentium voluptate perferendis ab, quae incidunt expedita obcaecati numquam consectetur quos eos nobis. Labore quaerat, officia vel dicta maxime, rerum, sequi eligendi quos culpa sapiente exercitationem repellat maiores voluptatibus aut. Recusandae sunt, error totam repudiandae unde, nisi reprehenderit, nesciunt similique animi reiciendis perspiciatis autem natus fugit excepturi asperiores! Ipsa, mollitia explicabo nisi aut perferendis at laborum, odio ipsum ipsam earum, omnis voluptatibus consectetur. Aperiam explicabo quas nemo. Nobis voluptate aliquam soluta, perspiciatis aut beatae, culpa, mollitia aspernatur maxime omnis praesentium nulla neque numquam inventore explicabo hic dolorum vero consectetur. Culpa, quam.</p>
+		</div>
+	</figure>
 
 	</section>
 
 	<section class="third-section">
+	<h1>Section Three</h1>
+
 		<p>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -50,6 +67,8 @@
 	</section>
 
 	<section class="fourth-section">
+	<h1>Section Four</h1>
+
 		<p>Vitae laudantium debitis, suscipit tempora voluptatibus molestias accusantium fuga doloremque ipsa, asperiores vero voluptatem labore provident. Ipsa explicabo, delectus assumenda omnis asperiores pariatur dicta rem ipsam alias nostrum velit voluptate, natus consequuntur. Possimus, suscipit, qui. Minus quaerat quis beatae mollitia accusamus saepe, tenetur excepturi? Quasi cum cupiditate nesciunt, similique fugit animi culpa unde sequi. Obcaecati esse deserunt, laudantium ab ducimus hic. Iure modi minima temporibus et dignissimos, fugit, amet nesciunt ut adipisci voluptate vero, reprehenderit architecto in consequatur perspiciatis facilis sunt sapiente inventore. Dolores sapiente magnam reprehenderit deleniti dolorem, error alias molestiae, provident eos similique veritatis non voluptates perferendis blanditiis. Iusto porro nostrum ducimus nulla sunt in eius iste soluta, id voluptate dolorem, consequuntur, laboriosam molestiae magni delectus qui dolorum quod fugit sit. Soluta voluptates commodi nobis, voluptatibus, temporibus assumenda pariatur optio vitae ipsum eos, odio. Aliquid voluptatem, libero amet est facilis asperiores dicta veniam quia animi at aspernatur expedita aut, assumenda esse placeat repellat magnam, totam, maxime voluptas ratione doloribus. Consequuntur molestias libero suscipit veritatis dolor accusantium tempora laborum deserunt necessitatibus, vero animi laboriosam asperiores iure adipisci fugiat, aperiam ipsam dicta natus autem odit tempore. Veniam maxime iste quo impedit consectetur eveniet maiores ab. Beatae vero ratione harum dolores.</p>
 	</section>
 
@@ -95,7 +114,7 @@
 		</p>
 	</section>
 
-<?php include "footer.php" ?>
+<?php include "footer-vid-index.php" ?>
 
 
 
